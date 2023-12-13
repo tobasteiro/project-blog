4a01830ae7bfe99311daa8a3f3c02ba7410680cb
Tobias Basteiro <tobiasbasteiro@Tobiass-MacBook-Pro.local>
2023-12-13 13:35:49 -0500

    project blog: 1 to 5.a exercises

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React from "react";
+import dynamic from "next/dynamic";
 
-import BlogHero from '@/components/BlogHero';
+import BlogHero from "@/components/BlogHero";
 
-import styles from './postSlug.module.css';
+import { loadBlogPost } from "@/helpers/file-helpers";
 
-function BlogPost() {
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+import styles from "./postSlug.module.css";
+import CodeSnippet from "@/components/CodeSnippet";
+
+const DivisionGroupsDemo = dynamic(() =>
+  import("@/components/DivisionGroupsDemo")
+);
+
+export const getBlogPostData = React.cache(
+  async (slug) => await loadBlogPost(slug)
+);
+
+export async function generateMetadata({ params }) {
+  const file = await getBlogPostData(params.postSlug);
+  return {
+    title: file.frontmatter.title,
+    description: file.frontmatter.abstract,
+  };
+}
+
+async function BlogPost({ params }) {
+  const file = await getBlogPostData(params.postSlug);
+
+  let addedSnippets = file.content
+    .replaceAll("```js\n", "<CodeSnippet>\n```js\n")
+    .replaceAll("```\n", "```\n</CodeSnippet>\n");
   return (
     <article className={styles.wrapper}>
       <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
+        title={file.frontmatter.title}
+        publishedOn={file.frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote
+          source={addedSnippets}
+          components={{ CodeSnippet, DivisionGroupsDemo }}
+        />
       </div>
     </article>
   );
